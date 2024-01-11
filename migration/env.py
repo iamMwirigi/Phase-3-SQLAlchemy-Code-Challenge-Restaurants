a8325899547935bd80ead23b5978edddb3814ea3
mwi <mwirigimburugu81@gmail.com>
2024-01-11 18:51:19 +0300

    completed

--- migration/env.py
+++ migration/env.py
@@ -18,6 +18,8 @@ fileConfig(config.config_file_name)
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = None
+from restaurant import Base
+target_metadata=Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
